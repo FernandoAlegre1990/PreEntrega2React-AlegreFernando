24d0ebe388d7fd9ad0f8325a7256fcee95c868db
Fernando Alegre <fernandoalegre31@gmail.com>
2023-11-02 16:37:40 -0300

    trabajo terminado

--- src/components/NavBarComponent/NavBarComponent.jsx
+++ src/components/NavBarComponent/NavBarComponent.jsx
@@ -4,6 +4,9 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import CartWidgetComponent from '../CartWidgetComponent/CartWidgetComponent';
+import '../../index.css';
+
+
 
 
 
@@ -11,28 +14,30 @@ const NavBarComponent = () => {
   return (
     <Navbar expand="lg" bg="#000" className="custom-navbar">
       <Container>
-        <Navbar.Brand href="#home" className="custom-brand">Salchimania Store</Navbar.Brand>
+        <Navbar.Brand href="#home" className="custom-brand">
+          <img
+            src={`assets/logo3.jpg`}
+            alt="Logo de Salchimania Store"
+            className="brand-image"
+          />
+          Salchimania Store
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home " className="custom-brand">About us</Nav.Link>
-            <Nav.Link href="#link" className="custom-brand">Shop</Nav.Link>
+          <Nav className="ml-auto"> {/* Usa ml-auto para alinear los enlaces a la derecha */}
+            <Nav.Link href="#home" className="custom-links">About us</Nav.Link>
+            <Nav.Link href="#link" className="custom-links">Shop</Nav.Link>
             <NavDropdown title="Categorias" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Ropa</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Accesorios
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Accesorios</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Alimento</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Contacto
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Contacto</NavDropdown.Item>
             </NavDropdown>
             <div className="cart-container">
-                 <CartWidgetComponent />
-           </div>
+              <CartWidgetComponent />
+            </div>
           </Nav>
-      
         </Navbar.Collapse>
       </Container>
     </Navbar>
